a8763138b3cff4e9540c829ea992299d8cd69832
Ashish <bashish1989@gmail.com>
2014-10-25 11:20:00 +0530

    changed width

--- desktop/src/com/abgamerguys/libgdx/demo/desktop/DesktopLauncher.java
+++ desktop/src/com/abgamerguys/libgdx/demo/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "My Demo Game";
-		config.width = 500;
+		config.width = 600;
 		config.height = 400;
 		new LwjglApplication(new MyDemo(), config);
 	}
